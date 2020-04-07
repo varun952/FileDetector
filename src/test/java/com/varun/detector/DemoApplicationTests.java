25b869b74cf4bbaf58460bca727b3fe54c8ffaec
Varun Singh <varun@karthavya.com>
2020-04-08 03:19:31 +0530

    Added detector project

---
+++ src/test/java/com/varun/detector/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
